f111edf19719e58e9d553a335c595be3a8dcfcf3
Jason Widdowson <jason.widdowson@employment.gov.au>
2021-04-25 11:52:02 +1000

    Routing for not found AB#3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./forms/forms.module').then(m => m.FormsModule) },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
